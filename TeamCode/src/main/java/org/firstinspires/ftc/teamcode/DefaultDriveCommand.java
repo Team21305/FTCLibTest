d18c43cb7e2d85255ea00a1bb278d3ccb968f32c
Nick Manor <nmanor@saturnsys.com>
2022-09-26 15:53:54 -0500

    Add squaring of inputs to desensitize the controls.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DefaultDriveCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DefaultDriveCommand.java
@@ -9,20 +9,29 @@ public class DefaultDriveCommand extends CommandBase {
     private final DriveSubsystem _drive;
     private final DoubleSupplier _forward;
     private final DoubleSupplier _rotation;
+    private final boolean _squareInputs;
 
-    public DefaultDriveCommand(DriveSubsystem subsystem, DoubleSupplier forward, DoubleSupplier rotation){
+    public DefaultDriveCommand(DriveSubsystem subsystem, DoubleSupplier forward, DoubleSupplier rotation, boolean squareInputs){
 
         _drive = subsystem;
         _forward = forward;
         _rotation = rotation;
+        _squareInputs = squareInputs;
 
         addRequirements(_drive);
-
     }
 
     @Override
     public void execute(){
-        _drive.drive(_forward.getAsDouble(), _rotation.getAsDouble());
+
+        double y = _forward.getAsDouble();
+        double r = _rotation.getAsDouble();
+        if(_squareInputs) {
+            y = Math.pow(_forward.getAsDouble(), 2) * Math.signum(_forward.getAsDouble());
+            r = Math.pow(_rotation.getAsDouble(), 2) * Math.signum(_rotation.getAsDouble());
+        }
+
+        _drive.drive(y, r);
 
     }
 
